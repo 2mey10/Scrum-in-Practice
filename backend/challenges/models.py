108025f4ea588d952548551b40c489b3e49058f2
Robert <robert.eichner@st.ovgu.de>
2023-03-13 13:29:43 +0100

    model for challenge

--- backend/challenges/models.py
+++ backend/challenges/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Challenge(models.Model):
+    class Metric(models.TextChoices):
+        ACCURACY = "1", "accuracy"
+        PRECISION = "2", "precision"
+        RECALL = "3", "recall"
+
+    description_text = models.CharField(max_length=500)
+    title_text = models.CharField(max_length=50)
+    dataset_url = models.URLField()
+    metric_choice = models.TextField(choices=Metric.choices)
