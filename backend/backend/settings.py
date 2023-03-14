dcc8504b9e2099bf5418186ede3544d175a9b459
Robert <robert.eichner@st.ovgu.de>
2023-03-14 09:59:17 +0100

    added Reststuff

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,3 +126,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
