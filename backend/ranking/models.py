84ff12feee5e7fa35f0e2903dc0e58276c588d27
Robert <robert.eichner@st.ovgu.de>
2023-03-14 13:58:49 +0100

    model entry v1

--- backend/ranking/models.py
+++ backend/ranking/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
+from challenges.models import Metric
 
 # Create your models here.
 
@@ -10,3 +11,11 @@ class MlModel(models.Model):
 
     def __str__(self):
         return "ml_model"
+
+
+class RankingModel(models.Model):
+    username = models.CharField(max_length=100, blank=True, null=True)
+    model_ref = models.ForeignKey(MlModel, on_delete=models.CASCADE,  blank=True, null=True)
+    used_metric = models.ForeignKey(Metric, on_delete=models.RESTRICT,  blank=True, null=True)
+    metric_score = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+
--- backend/ranking/views.py
+++ backend/ranking/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from . import models
 from . import serializers
 
+
 class MlModelViewSet(viewsets.ModelViewSet):
     queryset = models.MlModel.objects.all()
     serializer_class = serializers.MlModelSerializer
+
+
+class MakeEntry(APIView):
+    def get(self, request, format=None):
+        return Response("T")
+
