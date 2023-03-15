18cfda8cabd2a58af27b7448d3fa48ac43e85a0e
2mey10 <th.botschen@gmail.com>
2023-03-15 22:02:31 +0100

    cool homepage

--- backend/registration/serializers.py
+++ backend/registration/serializers.py
@@ -71,37 +71,13 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         # Add custom claims
         token['username'] = user.username
         token['email'] = user.email
+        token['firstname'] = user.firstname
         # ...
         return token
 
-# class RegisterSerializer(serializers.ModelSerializer):
-#     password = serializers.CharField(
-#         write_only=True, required=True, validators=[validate_password])
-#     password2 = serializers.CharField(write_only=True, required=True)
-#
-#     class Meta:
-#         model = User
-#         fields = ('username', 'password', 'password2')
-#
-#     def validate(self, attrs):
-#         if attrs['password'] != attrs['password2']:
-#             raise serializers.ValidationError(
-#                 {"password": "Password fields didn't match."})
-#
-#         return attrs
-#
-#     def create(self, validated_data):
-#         user = User.objects.create(
-#             username=validated_data['username']
-#         )
-#
-#         user.set_password(validated_data['password'])
-#         user.save()
-#
-#         return user
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
+        write_only=True, required=True)
     email = serializers.EmailField(
             required=True,
             validators=[]
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,34 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import {Link, useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import AuthContext from "../context/AuthContext";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 function Home() {
+    const { user, logoutUser } = useContext(AuthContext);
+    console.log(user)
+
+    const customGreeting = () => {
+        return user?(
+            <>
+                <Typography variant={"h3"}>
+                    Willkommen {user.username} zur Magdeburger Machine Learning Meisterschaft!
+                </Typography>
+            </>) : (
+            <>
+                <Typography variant={"h3"}>
+                    Willkommen zur Magdeburger Machine Learning Meisterschaft!
+                    Logge dich ein um fortzufahren.
+                </Typography>
+            </>
+        )
+    }
     return (
         <div>
-            <Typography variant={"h1"}>
-                This is the default home page!
-            </Typography>
+
+            {customGreeting()}
+
         </div>
     );
 }
