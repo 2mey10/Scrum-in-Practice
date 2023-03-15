569e0d489ec20df23eefa4d15bb1fe16ed3cafec
rami-alfish <ramizx1995@gmail.com>
2023-03-15 15:01:57 +0100

    evalutionModel

--- backend/ranking/models.py
+++ backend/ranking/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
-from challenges.models import Metric
-
+from challenges.models import Metric,Challenge
 # Create your models here.
 
 
@@ -14,8 +13,14 @@ class MlModel(models.Model):
 
 
 class RankingModel(models.Model):
-    username = models.CharField(max_length=100, blank=True, null=True)
     model_ref = models.ForeignKey(MlModel, on_delete=models.CASCADE,  blank=True, null=True)
     used_metric = models.ForeignKey(Metric, on_delete=models.RESTRICT,  blank=True, null=True)
     metric_score = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+    username = models.CharField(max_length=100, blank=True, null=True)
+    Challengid = models.ForeignKey(Challenge, on_delete=models.RESTRICT,  blank=True, null=True)
+    modelname = models.CharField(max_length=100, blank=True, null=True)
+    Accuracy = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+    Precision = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+    Recall = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+    F1 = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
 
