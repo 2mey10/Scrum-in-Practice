8dda6ef76b34950a2fe85578b7762e1a602dde92
Robert <robert.eichner@st.ovgu.de>
2023-03-15 15:13:48 +0100

    Change rankinglist

--- backend/ranking/views.py
+++ backend/ranking/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
@@ -10,7 +11,7 @@ from . import models
 from . import serializers
 from .models import MlModel
 from .models import RankingModel
-
+from .evalScript import start_evaluation
 
 class MlModelViewSet(viewsets.ModelViewSet):
     queryset = models.MlModel.objects.all()
@@ -22,10 +23,26 @@ class RankingEntryViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RankingEntrySerializer
 
 
-class MakeEntry(APIView):
-    def get(self, request, format=None):
-        queryset = models.MlModel.objects.all()
-        serializer_class = serializers.MlModelSerializer(queryset)
-        return Response(serializer_class.data)
+#class MakeEntry(APIView):
+ #   def get(self, request, format=None):
+ #       queryset = models.MlModel.objects.all()
+ #       serializer_class = serializers.MlModelSerializer(queryset)
+ #       return Response(serializer_class.data)
 
+
+@api_view(['GET'])
+def makeEntry(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    queryset = models.MlModel.objects.all()[1]
+    serializer_class = serializers.MlModelSerializer(queryset)
+
+    print(queryset)
+    score = start_evaluation(queryset, "Klassifizierung", "")
+
+    #queryset = models.MlModel.objects.all()[1]
+    #serializer_class = serializers.MlModelSerializer(queryset)
+
+    return Response(serializer_class.data)
 
