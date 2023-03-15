515cc3f052ab57f496177e34b3909eba3a98a1d7
Robert <robert.eichner@st.ovgu.de>
2023-03-15 17:03:06 +0100

    finally evaluation

--- backend/ranking/models.py
+++ backend/ranking/models.py
@@ -14,9 +14,9 @@ class MlModel(models.Model):
 
 
 class RankingModel(models.Model):
-    username = models.CharField(max_length=100, blank=True, null=True)
     model_id = models.IntegerField(blank=True, null=True)
     challenge_id = models.IntegerField(blank=True, null=True)
+    username = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.username + ": " + self.model_id
@@ -25,7 +25,7 @@ class RankingModel(models.Model):
 class RankingEntry(models.Model):
     model_ref = models.ForeignKey(MlModel, on_delete=models.CASCADE, blank=True, null=True)
     username = models.CharField(max_length=100, blank=True, null=True)
-    Challengid = models.ForeignKey(Challenge, on_delete=models.RESTRICT, blank=True, null=True)
+    Challengid = models.IntegerField(blank=True, null=True)
     modelname = models.CharField(max_length=100, blank=True, null=True)
     Accuracy = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
     Precision = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
--- backend/ranking/views.py
+++ backend/ranking/views.py
@@ -13,6 +13,9 @@ from .models import MlModel
 from .models import RankingModel
 from .evalScript import start_evaluation
 
+from django.conf import settings
+import os
+
 class MlModelViewSet(viewsets.ModelViewSet):
     queryset = models.MlModel.objects.all()
     serializer_class = serializers.MlModelSerializer
@@ -30,19 +33,37 @@ class RankingEntryViewSet(viewsets.ModelViewSet):
  #       return Response(serializer_class.data)
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def makeEntry(request):
     """
     List all code snippets, or create a new snippet.
     """
-    queryset = models.MlModel.objects.all()[1]
-    serializer_class = serializers.MlModelSerializer(queryset)
+    model_id = request.data.get("ml_model_id")
 
-    print(queryset)
-    score = start_evaluation(queryset, "Klassifizierung", "")
+    queryset = models.MlModel.objects.values_list('ml_model')
+
+    #serializer_class = serializers.MlModelSerializer(queryset)
+
+    score = start_evaluation(str(settings.MEDIA_ROOT) +  "/" +  queryset[model_id][0], "Klassifizierung", "")
 
     #queryset = models.MlModel.objects.all()[1]
     #serializer_class = serializers.MlModelSerializer(queryset)
 
+    #seril = serializers.RankingModelSerializer(data=request.data)
+
+    print(request.data)
+
+    rank_query = models.RankingEntry(model_ref = models.MlModel.objects.all()[model_id],
+    username = request.data.get("username"),
+    Challengid = request.data.get("challenge_id"),
+    modelname = queryset[model_id][0][9:],
+    Accuracy = score.get("accuracy"),
+    Precision = score.get("precision"),
+    Recall = score.get("recall"),
+    F1 = score.get("F1"),
+    is_human = True)
+    rank_query.save()
+
+    serializer_class = serializers.RankingEntrySerializer(rank_query)
     return Response(serializer_class.data)
 
