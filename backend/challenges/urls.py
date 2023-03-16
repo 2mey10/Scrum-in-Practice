66f3ff6432b1b34345697fe97d315c140566bbe7
rami-alfish <ramizx1995@gmail.com>
2023-03-16 10:22:10 +0100

    downlaodtry1

--- backend/challenges/urls.py
+++ backend/challenges/urls.py
@@ -5,10 +5,12 @@ from rest_framework.routers import DefaultRouter
 from .views import RolesViewSet
 from .views import CoursesViewSet
 from .views import MetricViewSet
+from .views import DownloadTrainDataView
 
 router = DefaultRouter()
 router.register('challenge', QuestionViewSet, basename='challenge')
 router.register('roles', RolesViewSet, basename='roles')
 router.register('courses', CoursesViewSet, basename='courses')
 router.register('metric', MetricViewSet, basename='metric')
+router.register('challenges/<int:pk>/train-data/', DownloadTrainDataView, basename='downloadtrain')
 urlpatterns = router.urls
